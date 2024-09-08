0968fe52683a25f17bd2d341cbb1b2fe977a044a
Michael Strobel <mstrobel97@gmail.com>
2024-09-08 22:11:33 +0200

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+  in {
+    devShells.${system}.default = let
+      pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell {
+      name = "composer-devShell";
+      buildInputs = [
+        pkgs.php83Packages.composer
+        pkgs.php83
+      ];
+    };
+  };
+}
